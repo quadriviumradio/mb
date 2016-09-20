4df75e6a2edaaf7cb76c6f9422734ac041dfc16f
quadriviumradio <quadriviumradio@gmail.com>
2016-09-20 18:22:41 -0400

    Création du fichier json

---
+++ odechets.js
@@ -0,0 +1,63 @@
+var articles = [ 
+	  {
+      "type": 3,
+      "titre": "Acheter en vrac à Montréal",
+      "id":"chapo",
+	  "chapo": "Il parait que bientôt, dans l'océan il y aura plus de sacs en plastique que de poissons. Aïe, quand on y pense, ça fait mal. Si on commençait par éviter tous ces plastiques qui recouvrent inutilement n'importe quel concombre de grande surface? Vous ne savez pas comment commencer? On vous explique comment s'initier à l'achat en vrac.",
+	  "tags": ["planète", "pratique", "bouffe"],
+	  "date": "Jeudi 29 septembre",
+	  "img": "../img/photo/sac.png"
+	  },
+	  {
+      "type": 5,
+      "titre": "Guide d'un achat zéro déchet",
+      "id":"guide",
+	  "chapo": "Andréanne, de la boutique Loco nous donne ses 4 conseils pour débuter sans avoir la tête...en vrac (#pardon). ",
+	  "date": "15 juin",
+	  "img": "../img/photo/bulle1.png",
+	  "adresse": "22 avenue Atwater",
+	  "ville": "Montréal", 
+	  "pos": {},
+	  "lien": "https://www.facebook.com/canallounge/",
+	  "texte":"1/Commencez par déterminer ce qui chez vous consomme le plus d'emballage. Ce sont les pâtes? Commencez par là !<br/><br/>2/Récupérez les contenants en verre (pots de confiture etc). Ne gardez pas les trop grands; les bocaux doivent se manipuler facilement, et gardez les à l'abri de la lumière. Pour faire vos courses, munissez-vous de vos bocaux et de tote bags (des sacs en tissu réutilisables.)<br/><br/>3/ Faites vous une base de produits de tous les jours : huiles, lentilles, et farine.<br/><br/>4/Organisez vos menus pour la semaine, en faisant le point sur ce que vous avez déjà à la maison." 
+	  },
+	  {
+      "type": 6,
+      "titre": "Epicerie Loco!",
+	  "chapo": "Toute l'épicerie, en vrac.",
+	  "tags": ["épicerie", "jarry"],
+	  "date": "15 avril",
+	  "img": "https://www.epicerieloco.ca/wp-content/uploads/2016/07/LOCO_EPICERIE_ECOLOGIQUE_ZERO_DECHET_COMMENT_CA_MARCHE_LIGHT.jpg",
+	  "adresse": "422 Jarry Est",
+	  "ville": "Montréal", 
+	  "pos": {lat: 45.482844, lng: -73.575670},
+	  "lien": "http://www.epicerieloco.ca/",
+	  "texte":"A deux pas du métro Jarry cette place réussit le pari de regrouper toute l'épicerie (produits lavants compris) et tout est entièrement bio. Pas besoin de courir ailleurs pour acheter du poisson, de la viande ou les céréales. Un joli pari signé de quatre amies, qui veulent changer le monde... un panier à la fois.<br/><br/>Dans la boutique vous trouverez même du poisson. S'il n'est pas labellisé bio, il est néanmoins issu de la pêche durable uniquement. Et si vous êtes tannée de cuisiner, vous pourrez toujours trouver sur place des plats cuisinés (sans emballage, mais de dans grandes jarres), des biscuits ou même des bocaux prêts à être cuisinés. Le vrac, ce n'est finalement pas si compliqué!"
+	},
+      {
+      "type": 6,
+      "titre": "Megavrac",
+	  "chapo": "Une petite nouvelle dans le quartier Rosemont.",
+	  "tags": ["épicerie", "the", "cafe"],
+	  "date": "15 mars",
+	  "img": "../img/photo/megavrac.png",
+	  "adresse": "2649 Rue Masson",
+	  "ville": "Montréal", 
+	  "pos": {lat: 445.514694, lng: -73.563735},
+	  "lien": "https://www.facebook.com/Mega-Vrac-746412688812851/",
+	  "texte":"Ouvert depuis début septembre, Mega vrac attire déjà ses clients du quartier de Rosemont. Créée par Ahlem, cette épicerie est tout de même moins systématiquement bio, que sa consoeur. Il n'y a qu'à voir : d'ès l'entrée, trônent en majesté des tonnes de bonbons et même une machine à pop corn. Mais le tout : en  vrac. <br/><br/> Mais mega vrac propose aussi tous les produits possibles et imaginables sans plastique. La preuve? On a même vu de la poudre de caroube.Vous ne connaissez pas? NOus non plus, mais Ahlem, la propriétaire des lieux aura toujours la bonne explication à vous fournir. Avec le sourire!"
+      },
+      {
+      "type": 5,
+      "titre": "Vrac et Bocaux",
+	  "chapo": "La future nouvelle adresse du vrac dans Rosemont. Ouverture début octobre 2016.",
+	  "tags": ["vrac", "epicerie", "Rosemont"],
+	  "date": "15 septembre",
+	  "img": "http://i0.wp.com/www.minibulles.ca/wp-content/uploads/2015/10/bar-872161_1280.jpg",
+	  "adresse": "6698 Ave Christophe-Colomb",
+	  "ville": "Montréal", 
+	  "pos": {lat: 45.539065, lng: -73.605006},
+	  "lien": "https://www.facebook.com/vracetbocauxepiceriebio/",
+	  "texte":"À seulement quelques minutes à pied du métro Beaubien, cette nouvelle épicerie sera facile d'accès en vélo grâce à la piste cyclable et en auto avec le stationnement à proximité. Encore un peu de patience, nous vous dévoilerons la date d'ouverture très bientôt"
+      }
+      ]
